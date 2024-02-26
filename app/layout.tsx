4c4520d2a0f72adb4c7ea14ea013443f7cbf0777
Alisina-Adeel <79627395+Alisina-Adeel@users.noreply.github.com>
2024-02-27 00:05:57 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,25 +8,24 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "Alisina Adeel",
+  description: "This is my portfilio",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
-      >
+      <body className={`${inter.className} bg-[#030012] overflow-y-scroll overflow-x-hidden`}>
         <StarsCanvas />
+        
         <Navbar />
         {children}
         <Footer />
-      </body>
+        </body>
     </html>
   );
 }
